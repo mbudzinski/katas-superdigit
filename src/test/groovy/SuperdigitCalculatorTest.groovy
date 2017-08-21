61005599e50c02cce9733b33afe04dd0f7427036
marcinbudzinski <marcin.budzinski@hrs.com>
2017-08-21 13:26:58 +0200

    Added missing description to superdigitCalculatorTest

--- src/test/groovy/SuperdigitCalculatorTest.groovy
+++ src/test/groovy/SuperdigitCalculatorTest.groovy
@@ -16,8 +16,6 @@ class SuperdigitCalculatorTest extends Specification {
             input << ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     }
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     def 'The result is always a single digit'() {
         given:
             def superdigitCalculator = new SuperdigitCalculator()
@@ -31,10 +29,8 @@ class SuperdigitCalculatorTest extends Specification {
             input << ["35763563", "542542", "246554367654874", "25346745254385", "9769567845",
                       "123143215", "1111111111", "10000000", "8487548", "21"]
     }
-=======
-=======
->>>>>>> Stashed changes
-    def ''() {
+
+    def 'When the number of superdigit cycles is the same for the input and incremented input then the result from incremented input is larger by 1'() {
         given:
             def superdigitCalculator = new SuperdigitCalculator()
         when:
@@ -50,13 +46,7 @@ class SuperdigitCalculatorTest extends Specification {
             imperativeIncrementedResult == imperativeResult + 1
             functionalResult == imperativeResult
             imperativeIncrementedResult == functionalIncrementedResult
-
-
         where:
             input << ['222', '111', '521']
     }
-
-
-
->>>>>>> Stashed changes
 }
